31c03cc8ce47e1f93b91b13edac6badbb58e8800
jwroush4 <73079062+jwroush4@users.noreply.github.com>
2020-10-18 19:47:35 -0400

    llr_functions wz

--- llr_functions.R
+++ llr_functions.R
@@ -21,7 +21,7 @@ llr <- function(x, y, z, omega) {
 #' @param omega (numeric) must be a scalar
 #' @return (numeric) scalar
 compute_f_hat <- function(z, x, y, omega) {
-  Wz <- make_weight_matrix(z, x, omega)
+  Wz <- diag(z, x, omega)
   X <- make_predictor_matrix(x)
   f_hat <- c(1, z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
   return(f_hat)
