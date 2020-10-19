a278fe318efd37bbbabd5d47c6f29c751d41c730
jwroush4 <73079062+jwroush4@users.noreply.github.com>
2020-10-19 18:23:18 -0400

    another fix for llr functions in speed test 1

--- llr_functions.R
+++ llr_functions.R
@@ -23,7 +23,7 @@ llr <- function(x, y, z, omega) {
 compute_f_hat <- function(z, x, y, omega) {
   Wz <- diag(z, x, omega)
   X <- make_predictor_matrix(x)
-  f_hat <- c(1, z) %*% solve(t(X) %*% apply(X,2,function(v) v*Wz) %*% t(X) %*% Wz*y
+  f_hat <- c(1, z) %*% solve(t(X) %*% apply(X,2,function(v) v*Wz)) %*% t(X) %*% Wz*y
   return(f_hat)
 }
 
